17600dbef0a20e95e1e2d03d1925dbf074e3ff50
Justin Bois <bois@caltech.edu>
2015-11-28 15:40:57 -0800

    Added Costes localization

--- bebi103.py
+++ bebi103.py
@@ -5,11 +5,14 @@ Author: Justin Bois
 """
 
 import collections
+import random
 import warnings
 
 import matplotlib.path as path
 import numpy as np
 import pandas as pd
+import scipt.odr
+import scipy.stats as st
 
 import skimage.io
 import skimage.measure
@@ -375,7 +378,7 @@ def run_ensemble_emcee(log_post, n_burn, n_steps, n_walkers=None, p_dict=None,
         call signature f(*args_for_function, n_walkers).  Ignored
         if p0 is not None.
     p0 : array
-        n_dim by n_walkers array of initial starting values.
+        n_walkers by n_dim array of initial starting values.
         p0[i,j] is the starting point for walk i along variable j.
         If provided, p_dict is ignored.
     columns : list of strings
@@ -487,7 +490,7 @@ def run_pt_emcee(log_like, log_prior, n_burn, n_steps, n_temps=None,
         call signature f(*args_for_function, n_walkers).  Ignored
         if p0 is not None.
     p0 : array
-        n_dim by n_walkers array of initial starting values.
+        n_walkers by n_dim array of initial starting values.
         p0[i,j] is the starting point for walk i along variable j.
         If provided, p_dict is ignored.
     columns : list of strings
@@ -847,3 +850,342 @@ def verts_to_roi(verts, size_i, size_j):
     # Return boolean in same shape as image
     return (roi, roi_bbox, roi_box)
 
+
+class CostesColocalization(object):
+    """
+    Generic class just to store attributes
+    """
+    def __init__(self, **kw):
+        self.__dict__ = kw
+
+
+def costes_coloc(im_1, im_2, psf_width=3, n_scramble=1000, thresh_r=0.0,
+                 roi=None, roi_method='all', do_manders=True):
+    """
+    Perform Costes colocalization analysis on a pair of images.
+
+    Parameters
+    ----------
+    im_1: array_like
+        Intensity image for colocalization.  Must be the
+        same shame as `im_1`.
+    im_2: array_like
+        Intensity image for colocalization.  Must be the
+        same shame as `im_2`.
+    psf_width: int, default 3
+        Width, in pixels of the point spread function.
+    n_scramble: int, default 1000
+        Number of strambled image comparisons to do to get statistics.
+    thresh_r: float, default 0.0
+        Threshold Pearson r value to be considered colocalized.
+    roi: array_like, dtype bool, default None
+        Boolean image the same shape as `im_1` and `im_2` that
+        is True for pixels within the ROI.
+    roi_method: str, default 'all'
+        If 'all', all pixels of a given subimage must be within
+        the ROI for the subimage itself to be considered part
+        of the ROI.  If 'any', if any one pixel is within the ROI,
+        the subimage is considered part of the ROI.
+    do_manders: bool, default True
+        If True, compute the Manders coefficients.
+
+    Returns
+    -------
+    output: A CostesColocalization instance.
+        The CostesColocalization instance has the following attributes.
+            im_1, im_2, psf_width, n_scramble, thresh_r, roi,
+                roi_method: As in the input parameters.
+            a: slope of the regression line I_2 = a * I_1 + b
+            b: intercept of regression line I_2 = a * I_1 + b
+            M_1: Manders coefficient for image 1
+            M_2: Manders coefficient for image 2
+            pearson_r: Pearson coerrelaction coefficient of the pixels
+                in the two images.
+            p_coloc: The probability of colocalization being present
+                in the two images.
+    """
+
+    # Make mirrored boundaries in preparation for scrambling
+    im_1_mirror = mirror_edges(im_1, psf_width)
+    im_2_mirror = mirror_edges(im_2, psf_width)
+
+    # Set up ROI
+    if roi is None:
+        roi = np.ones_like(im_1, dtype='bool')
+
+    # Rename images to be sliced ROI
+    im_1 = im_1[roi]
+    im_2 = im_2[roi]
+
+    # Mirror ROI at edges
+    roi_mirror = mirror_edges(roi, psf_width)
+
+    # Compute the blocks that we'll scramble
+    blocks_1 = im_to_blocks(im_1_mirror, psf_width, roi_mirror, roi_method)
+    blocks_2 = im_to_blocks(im_2_mirror, psf_width, roi_mirror, roi_method)
+
+    # Flatten second list of blocks for Pearson calculations
+    blocks_2_flat = np.array(blocks_2).flatten()
+
+    # Compute the Pearson coefficient
+    pearson_r, _ = st.pearsonr(np.array(blocks_1).ravel(), blocks_2_flat)
+
+    # Do image scrambling and r calculations
+    r_scr = np.empty(n_scramble)
+    for i in range(n_scramble):
+        random.shuffle(blocks_1)
+        r, _ = scipy.stats.pearsonr(np.array(blocks_1).ravel(), blocks_2_flat)
+        r_scr[i] = r
+
+    # Compute percent chance of coloc
+    p_coloc = (r_scr < pearson_r).sum() / n_scramble
+
+    # Now do work to compute adjusted Manders's coefficients
+    if do_manders:
+        # Get the linear relationship between im_2 and im_1
+        a, b = odr_linear(im_1.ravel(), im_2.ravel())
+
+        # Perform threshold calculation
+        thresh_1 = find_thresh(im_1, im_2, a, b, thresh_r=thresh_r)
+        thresh_2 = a * thresh_1 + b
+
+        # Compute Costes's update to the Manders's coefficients
+        inds = (im_1 > thresh_1) & (im_2 > thresh_2)
+        M_1 = im_1[inds].sum() / im_1.sum()
+        M_2 = im_2[inds].sum() / im_2.sum()
+
+        # Toss results into class for returning
+        return CostesColocalization(
+            psf_width=psf_width, n_scramble=n_scramble, thresh_r=thresh_r,
+            thresh_1=thresh_1, thresh_2=thresh_2, a=a, b=b, M_1=M_1,
+            M_2=M_2, r_scr=r_scr, pearson_r=pearson_r, p_coloc=p_coloc)
+    else:
+        return CostesColocalization(
+            psf_width=psf_width, n_scramble=n_scramble, thresh_r=None,
+            thresh_1=None, thresh_2=None, a=None, b=None, M_1=None,
+            M_2=None, r_scr=r_scr, pearson_r=pearson_r, p_coloc=p_coloc)
+
+
+def odr_linear(x, y, intercept=None, beta0=None):
+    """
+    Performs orthogonal linear regression on x, y data.
+
+    Parameters
+    ----------
+    x: array_like
+        x-data, 1D array.  Must be the same lengths as `y`.
+    y: array_like
+        y-data, 1D array.  Must be the same lengths as `x`.
+    intercept: float, default None
+        If not None, fixes the intercept.
+    beta0: array_like, shape (2,)
+        Guess at the slope and intercept, respectively.
+
+    Returns
+    -------
+    output: ndarray, shape (2,)
+        Array containing slope and intercept of ODR line.
+    """
+
+    def linear_fun(p, x):
+        return p[0] * x + p[1]
+
+    def linear_fun_fixed(p, x):
+        return p[0] * x + intercept
+
+    # Set the model to be used for the ODR fitting
+    if intercept is None:
+        model = scipy.odr.Model(linear_fun)
+        if beta0 is None:
+            beta0 = (0.0, 1.0)
+    else:
+        model = scipy.odr.Model(linear_fun_fixed)
+        if beta0 is None:
+            beta0 = (1.0,)
+
+    # Make a Data instance
+    data = scipy.odr.Data(x, y)
+
+    # Instantiate ODR
+    odr = scipy.odr.ODR(data, model, beta0=beta0)
+
+    # Perform ODR fit
+    try:
+        result = odr.run()
+    except scipy.odr.odr_error:
+        raise scipy.odr.odr_error('ORD failed.')
+
+    return result.beta
+
+
+def find_thresh(im_1, im_2, a, b, thresh_r=0.0):
+    """
+    Find the threshold pixel intensity of `im_1` where
+    the Pearson correlation between the images goes below `thresh_r`.
+
+    Parameters
+    ----------
+    im_1: array_like
+        Intensity image for colocalization.  Must be the
+        same shame as `im_1`.
+    im_2: array_like
+        Intensity image for colocalization.  Must be the
+        same shame as `im_2`.
+    a: float
+        Slope of the ORD regression of `im_2` vs. `im_1`.
+    b: float
+        Intercept of the ORD regression of `im_2` vs. `im_1`.
+    thresh_r: float, default 0.0
+        Threshold Pearson correlation
+
+    Returns
+    -------
+    output: int or float
+        The threshold pixel intensity for colocalization
+        (see notes below).
+
+    Notes
+    -----
+    ..  To determine which pixels are colocalized in two images, we
+        do the following:
+            1. Perform a regression based on all points of to give
+               I_2 = a * I_1 + b.
+            2. Define T = I_1.max().
+            3. Compute the Pearson r value considering all pixels with
+               I_1 < T and I_2 < a * T + b.
+            4. If r <= thresh_r decrement T and goto 3.  Otherwise,
+               save $T_1 = T$ and $T_2 = a * T + b.
+            5. Pixels with I_2 > T_2 and I_1 > T_1 are colocalized.
+        This function returns T.
+    """
+    if im_1.dtype not in [np.uint16, np.uint8]:
+        incr = (im_1.max() - im_1.min()) / 256.0
+    else:
+        incr = 1
+
+    thresh_max = im_1.max()
+    thresh_min = im_1.min()
+    thresh = thresh_max
+    r = pearsonr_below_thresh(thresh, im_1, im_2, a, b)
+    min_r = r
+    min_thresh = thresh
+    while thresh > thresh_min and r > thresh_r:
+        thresh -= incr
+        r = pearsonr_below_thresh(thresh, im_1, im_2, a, b)
+        if min_r > r:
+            min_r = r
+            min_thresh = thresh
+
+    if thresh == thresh_min:
+        thresh = min_thresh
+
+    return thresh
+
+
+def pearsonr_below_thresh(thresh, im_1, im_2, a, b):
+    """
+    The Pearson r between two images for pixel values below
+    threshold.
+
+    Parameters
+    ----------
+    thresh: float or int
+        The threshold value of pixel intensities to consider for
+        `im_1`.
+    im_1: array_like
+        Intensity image for colocalization.  Must be the
+        same shame as `im_1`.
+    im_2: array_like
+        Intensity image for colocalization.  Must be the
+        same shame as `im_2`.
+    a: float
+        Slope of the ORD regression of `im_2` vs. `im_1`.
+    b: float
+        Intercept of the ORD regression of `im_2` vs. `im_1`.
+    """
+    inds = (im_1 <= thresh) | (im_2 <= a * thresh + b)
+    r, _ = st.pearsonr(im_1[inds], im_2[inds])
+    return r
+
+
+def mirror_edges(im, psf_width):
+    """
+    Given a 2D image pads the boundaries by mirroring so that the
+    dimensions of the image are multiples for the width of the
+    point spread function.
+
+    Parameters
+    ----------
+    im: array_like
+        Image to mirror edges
+    psf_width: int
+        The width, in pixels, of the point spread function
+
+    Returns
+    -------
+    output: array_like
+        Image with mirrored edges
+    """
+
+    # How much we need to pad
+    pad_i = psf_width - (im.shape[0] % psf_width)
+    pad_j = psf_width - (im.shape[1] % psf_width)
+
+    # Get widths
+    pad_top = pad_i // 2
+    pad_bottom = pad_i - pad_top
+    pad_left = pad_j // 2
+    pad_right = pad_j - pad_left
+
+    # Do the padding
+    return np.pad(im, ((pad_top, pad_bottom), (pad_left, pad_right)),
+                  mode='reflect')
+
+
+def im_to_blocks(im, width, roi=None, roi_method='all'):
+    """
+    Converts image to list of square subimages called "blocks."
+
+    Parameters
+    ----------
+    im: array_like
+        Image to convert to a list of blocks.
+    width: int
+        Width of square blocks in units of pixels.
+    roi: array_like, dtype bool, default None
+        Boolean image the same shape as `im_1` and `im_2` that
+        is True for pixels within the ROI.
+    roi_method: str, default 'all'
+        If 'all', all pixels of a given subimage must be within
+        the ROI for the subimage itself to be considered part
+        of the ROI.  If 'any', if any one pixel is within the ROI,
+        the subimage is considered part of the ROI.
+
+    Returns
+    -------
+    output: list of ndarrays
+        Each entry is a `width` by `width` NumPy array containing
+        a block.
+    """
+    # Initialize ROI
+    if roi is None:
+        roi = np.ones_like(im)
+
+    # Specify method for determining if in ROI or not
+    if roi_method == 'all':
+        roi_test = np.all
+    else:
+        roi_test = np.any
+
+    # Construct list of blocks
+    return [im[i:i+width, j:j+width]
+                for i in range(0, im.shape[0], width)
+                    for j in range(0, im.shape[1], width)
+                        if roi_test(roi[i:i+width, j:j+width])]
+#     blocks = []
+#     for i in range(0, im.shape[0], width):
+#         for j in range(0, im.shape[1], width):
+#             if roi_test(roi[i:i+width, j:j+width]):
+#                 blocks.append(im[i:i+width, j:j+width])
+
+#     return blocks
