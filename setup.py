7ffcdf86e1ab65e19ad9463d4762055877af959c
Justin Bois <bois@caltech.edu>
2018-11-11 00:29:01 -0800

    Version 0.0.35

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '0.0.34'
+__version__ = '0.0.35'
 
 here = path.abspath(path.dirname(__file__))
 
