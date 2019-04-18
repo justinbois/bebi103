7f8c2a6858317f6fbd07640a1152414ee442d05a
Tom Morrell <tmorrell@caltech.edu>
2019-04-18 12:35:33 -0700

    Add codemeta file and prep for CaltechDATA

--- setup.py
+++ setup.py
@@ -1,8 +1,43 @@
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
+import json
 
-__version__ = '0.0.41'
+def read(fname):
+    with open(fname, mode = "r", encoding = "utf-8") as f:
+        src = f.read()
+    return src
+
+codemeta_json = "codemeta.json"
+
+# Let's pickup as much metadata as we need from codemeta.json
+with open(codemeta_json, mode = "r", encoding = "utf-8") as f:
+    src = f.read()
+    meta = json.loads(src)
+
+# Let's make our symvar string
+__version__ = meta["version"]
+
+# Now we need to pull and format our author, author_email strings.
+author = ""
+author_email = ""
+for obj in meta["author"]:
+    given = obj["givenName"]
+    family = obj["familyName"]
+    email = obj["email"]
+    if len(author) == 0:
+        author = given + " " + family
+    else:
+        author = author + ", " + given + " " + family
+    if len(author_email) == 0:
+        author_email = email
+    else:
+        author_email = author_email + ", " + email
+description = meta['description']
+url = meta['codeRepository']
+license = meta['license']
+name = meta['name']
+keywords= meta['keywords']
 
 here = path.abspath(path.dirname(__file__))
 
@@ -18,11 +53,11 @@ install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
 
 setup(
-    name='bebi103',
+    name=name,
     version=__version__,
-    description='Python utilities for the Caltech course BE/Bi 103: Data Analysis in the Biological Sciences.',
+    description=description,
     long_description=long_description,
-    url='https://github.com/justinbois/bebi103',
+    url=url,
     download_url='https://github.com/justinbois/bebi103/tarball/' + __version__,
     license='BSD',
     classifiers=[
@@ -30,11 +65,11 @@ setup(
       'Intended Audience :: Developers',
       'Programming Language :: Python :: 3',
     ],
-    keywords='',
+    keywords=keywords,
     packages=find_packages(exclude=['docs', 'tests*']),
     include_package_data=True,
-    author='Justin Bois',
+    author=author,
     install_requires=install_requires,
     dependency_links=dependency_links,
-    author_email='bois@caltech.edu'
+    author_email=author_email
 )
